7a46767db9334b55a52d95571097b9556237e919
Ben-vegabond <vega.ben18@gmail.com>
2023-05-30 18:39:24 -0500

    added the changes to the js for the pw gen

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 // Assignment code here
+//funciton to generate random password with the criteria 
+function generatePassword(){
+  const length = prompt('Enter the desired length of the password (between 8 and 128 characters):');
+  const includeLowercase = confirm('Include lowercase characters?');
+  const includeUppercase = confirm('Include uppercase characters?');
+  const includeNumeric = confirm('Include numeric characters?');
+  const includeSpecial = confirm('Include special characters?');
 
+  // Validate password length
+  if (length < 8 || length > 128) {
+    alert('INVALID!! FIX THE LENGTH! ');
+    return;
+  }
+
+
+  // Validate at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert('Select at least one character type...');
+    return;
+  }
+  
+   // Define character sets based on selected criteria
+   const lowercaseCharset = 'abcdefghijklmnopqrstuvwxyz';
+   const uppercaseCharset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   const numericCharset = '0123456789';
+   const specialCharset = '!@#$%^&*()-_=+';
+ 
+   let charset = '';
+   if (includeLowercase) {
+     charset += lowercaseCharset;
+   }
+   if (includeUppercase) {
+     charset += uppercaseCharset;
+   }
+   if (includeNumeric) {
+     charset += numericCharset;
+   }
+   if (includeSpecial) {
+     charset += specialCharset;
+   }
+ 
+   // Generate the password
+   let password = '';
+   for (let i = 0; i < length; i++) {
+     const randomIndex = Math.floor(Math.random() * charset.length);
+     password += charset[randomIndex];
+   }
+ 
+   return password;
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,6 +63,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
